8f8eaf023c45deb7ac194ce149f7665b9776cd5b
Raya Ashok <n16107@std.it-college.ac.jp>
2016-08-18 12:05:45 +0900

    exam2016

--- exam/test.py
+++ exam/test.py
@@ -1,6 +1,19 @@
-def test():
-    print('ok')
+import sys
+import unittest
+
+class TestTest(unittest.TestCase, StdIoHook):
+class StdIoHook:
+     def __init__(self):
+            self.held = None
+
+     def test_test(self):
+       test.test()
+       self.assertEqual(sys.stdout.getvalue(), 'ok\n')
 
 
-def test2():
-    print('ng')
+     def test_test2(self):
+       test.test()
+       self.assertEqual(sys.stdout.getvalue(), 'ok\n')
+       self.reset_stdout()
+       test.test2()
+       self.assertEqual(sys.stdout.getvalue(), 'ng\n')
